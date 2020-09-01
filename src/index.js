48f95bc94d50a800590b7a299d1f5fec4189f645
张文旗 <1071942338@qq.com>
2020-09-01 17:45:15 +0800

    添加root接口

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ const responseTemplate = require("./util/responseTemplate");
 const bodyParser = require("koa-bodyparser");
 
 const app = new Koa();
-const fs = require("fs");
-const Path = require("path");
+
 const cors = require("@koa/cors");
+const readDir = require("./util/readUtil").default;
 
 app.use(bodyParser());
 
@@ -37,9 +37,19 @@ app.use(
                 responseTemplate.fail.message = "缺少 path 参数";
                 response.body = responseTemplate.fail;
             } else {
-                const files = await readDir();
+                const files = await readDir.readDirAndFile(path);
                 // console.log("files2", files);
 
+                responseTemplate.success.data = files;
+                response.body = responseTemplate.success;
+            }
+        } else if (pathname === "/root") {
+            const { path } = queryObj;
+            if (path === undefined) {
+                responseTemplate.fail.message = "缺少 path 参数";
+                response.body = responseTemplate.fail;
+            } else {
+                const files = await readDir.readDir(path);
                 responseTemplate.success.data = files;
                 response.body = responseTemplate.success;
             }
@@ -49,42 +59,4 @@ app.use(
 
 app.listen(80);
 
-async function readDir() {
-    const files = await fs.promises.readdir(__dirname, { withFileTypes: true });
-    // console.log("promises:", files);
-    const infos = [];
-    for (const dirent of files) {
-        const info = await readFileInfo(dirent);
-        infos.push(info);
-    }
-    return infos;
-}
-
-async function readFileInfo(dirent) {
-    // console.log("readFileInfo:", dirent);
-    const info = {
-        isFile: false,
-        isDir: false,
-        name: null,
-        createTime: null,
-        updateTime: null,
-        size: null,
-    };
-
-    info.isFile = dirent.isFile();
-    info.isDir = dirent.isDirectory();
-    info.name = dirent.name;
-    info.path = Path.posix.join(__dirname, dirent.name);
-    // console.log("info:", info);
-
-    const stat = await fs.promises.stat(info.path);
-    // console.log("stats:", stat);
-
-    info.createTime = stat.birthtime;
-    info.updateTime = stat.mtime;
-    info.size = stat.size;
-    // const stats =
-    return info;
-}
-
-readDir();
+console.log("__dirname:", __dirname);
---
+++ src/util/readUtil.js
@@ -0,0 +1,67 @@
+const fs = require("fs");
+const Path = require("path");
+
+async function readFileInfo(path, dirent) {
+    // console.log("readFileInfo:", dirent);
+    const info = {
+        isFile: false,
+        isDir: false,
+        name: null,
+        createTime: null,
+        updateTime: null,
+        size: null,
+    };
+
+    info.isFile = dirent.isFile();
+    info.isDir = dirent.isDirectory();
+    info.name = dirent.name;
+    info.path = Path.posix.join(path, dirent.name);
+    // console.log("info:", info);
+
+    const stat = await fs.promises.stat(info.path);
+    // console.log("stats:", stat);
+
+    info.createTime = stat.birthtime;
+    info.updateTime = stat.mtime;
+    info.size = stat.size;
+    // const stats =
+    return info;
+}
+
+async function readDirAndFile(path) {
+    if (path === undefined || path === null) {
+        path = "/";
+    }
+    const files = await fs.promises.readdir(path, { withFileTypes: true });
+    console.log("files:", files);
+    const infos = [];
+    for (const dirent of files) {
+        if (dirent.isFile() || dirent.isDirectory()) {
+            // 只显示文件夹或者文件
+            const info = await readFileInfo(path, dirent);
+            infos.push(info);
+        }
+    }
+    return infos;
+}
+
+async function readDir(path) {
+    if (path === undefined || path === null) {
+        path = "/";
+    }
+    const files = await fs.promises.readdir(path, { withFileTypes: true });
+    console.log("files:", files);
+    const infos = [];
+    for (const dirent of files) {
+        if (dirent.isDirectory()) {
+            // 只显示文件夹
+            const info = await readFileInfo(path, dirent);
+            infos.push(info);
+        }
+    }
+    return infos;
+}
+
+exports.default = {
+    readDirAndFile,
+};
