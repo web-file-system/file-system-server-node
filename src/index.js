2c2f560a05b3946831cf096b89d532ba19bf2746
张文旗 <1071942338@qq.com>
2020-09-01 16:54:37 +0800

    yarn add koa-bodyparser

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 const Koa = require("koa");
 const mount = require("koa-mount");
 const URL = require("url");
-const querystring = require("querystring");
 const responseTemplate = require("./util/responseTemplate");
+const bodyParser = require("koa-bodyparser");
+
 const app = new Koa();
 const fs = require("fs");
-const path = require("path");
+const Path = require("path");
 const cors = require("@koa/cors");
+
+app.use(bodyParser());
 
 app.use(cors());
 
@@ -19,16 +22,27 @@ app.use(
     mount("/api", async (ctx) => {
         const request = ctx.request;
         const response = ctx.response;
+        // console.log("request:", request);
+        // console.log("request-body:", request.body);
 
         const url = URL.parse(request.url);
         const pathname = url.pathname;
 
-        if (pathname === "/list") {
-            const files = await readDir();
-            console.log("files2", files);
+        const queryObj = request.body;
+        // console.log("queryObj:", queryObj);
 
-            responseTemplate.success.data = files;
-            response.body = responseTemplate.success;
+        if (pathname === "/list") {
+            const { path } = queryObj;
+            if (path === undefined) {
+                responseTemplate.fail.message = "缺少 path 参数";
+                response.body = responseTemplate.fail;
+            } else {
+                const files = await readDir();
+                // console.log("files2", files);
+
+                responseTemplate.success.data = files;
+                response.body = responseTemplate.success;
+            }
         }
     })
 );
@@ -37,34 +51,39 @@ app.listen(80);
 
 async function readDir() {
     const files = await fs.promises.readdir(__dirname, { withFileTypes: true });
-    console.log("promises:", files);
+    // console.log("promises:", files);
     const infos = [];
-    files.forEach((dirent) => {
-        console.log("dirent:", dirent.isFile());
-        const info = readFileInfo(dirent);
+    for (const dirent of files) {
+        const info = await readFileInfo(dirent);
         infos.push(info);
-    });
+    }
     return infos;
 }
 
-function readFileInfo(dirent) {
-    console.log("readFileInfo:", dirent);
+async function readFileInfo(dirent) {
+    // console.log("readFileInfo:", dirent);
     const info = {
         isFile: false,
         isDir: false,
         name: null,
+        createTime: null,
+        updateTime: null,
+        size: null,
     };
 
     info.isFile = dirent.isFile();
     info.isDir = dirent.isDirectory();
     info.name = dirent.name;
-    info.path = path.posix.join(__dirname, dirent.name);
-    // const dirName = __dirname;
-    // console.log("dirName:", dirName);
-    // const filePath = path.join(dirName, fileName);
-    // console.log("filePath:", filePath);
-    // const fileInfo = await fs.promises.readdir(filePath);
-    console.log("info:", info);
+    info.path = Path.posix.join(__dirname, dirent.name);
+    // console.log("info:", info);
+
+    const stat = await fs.promises.stat(info.path);
+    // console.log("stats:", stat);
+
+    info.createTime = stat.birthtime;
+    info.updateTime = stat.mtime;
+    info.size = stat.size;
+    // const stats =
     return info;
 }
 
